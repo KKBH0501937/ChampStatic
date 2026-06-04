f45c87793d8e6582928ac2d27be825f29fe13c8c
Kouadio Brice <hermane1628@gmail.com>
2026-06-04 10:29:20 +0000

    Instanciation de la classe Personne afin de visualiser les valeurs des deux compteurs

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
 public class Main {
+    public static void main(String[] args){
+
+
+
+
+
+        // --- Création des instances ---
+        // Chaque appel à new Personne() :
+        //   - incrémente Personne.nbInstances (compteur de classe, partagé)
+        //   - incrémente nbLocal de l'objet nouvellement créé (compteur d'instance)
+
+
+        Personne personne1 = new Personne();
+
+        Personne personne2 = new Personne();
+
+        Personne personne3 = new Personne();
+
+        Personne personne4 = new Personne();
+
+        // Affichez les valeurs des deux compteurs
+
+        System.out.println("(" + personne4.nbLocal + "," + Personne.nbInstances + ")");
+
+    }
 }
